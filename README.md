87b74090773f8bd3609332ca186ef02a2c518eaf
Charumehra <charumehracm2004@gmail.com>
2025-01-29 13:15:10 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
-# spotify-clone
-music website 
- <br>
-by Charu Mehra
+# Spotify Clone
+
+A music website built with HTML, CSS, and JavaScript.
+
+## Overview
+
+This is a clone of the popular music streaming service, Spotify. It allows users to browse through a library of songs, create playlists, and listen to music online.
+
+## Features
+
+* Browse through a library of songs
+* Create and manage playlists
+* Listen to music online
+
+## Screenshots
+
+![Screenshot of the website](https://i.imgur.com/3Jh7WkV.png)
+
+## Technologies Used
+
+* HTML
+* CSS
+* JavaScript
+
+## Development
+
+To develop this project, you will need to have Node.js installed on your computer. You can download it from [here](https://nodejs.org/en/download/).
+
+After you have Node.js installed, you can clone this repository and run the following commands in the terminal:
--- script.js
+++ script.js
@@ -1,77 +1,117 @@
-console.log("lets write some javascript");
-
-let currentSongIndex = 0;
+// Global Variables
 let audioPlayer = new Audio();
 let isPlaying = false;
-let songsList = [
-    'Aaj Ki Raat - Stree.mp3',
-    'Aayi Nai - Stree.mp3',
-    'audio1.mp3',
-    'Bhool Bhulaiyaa 3 - Title Track (Feat. Pitbull) - Bhool Bhulaiyaa 3 .mp3',
-    'Phir Se Ud Chala - Rockstar .mp3',
-    'Hauli Hauli - Khel Khel Mein .mp3',
-    'In Love - Guru Randhawa .mp3',
-    'Ittar - Jasmine Sandlas .mp3',
-    'Kissik - Pushpa 2 The Rule .mp3',
-    'Mera Na - Sidhu Moose Wala .mp3',
-    'Saanware - Akhil Sachdeva .mp3'
+let currentSongIndex = 0;
+
+// Updated Songs List
+const songsList = [
+    'songsplaylist/Aaj Ki Raat - Stree 2 320 Kbps.mp3',
+    'songsplaylist/Aayi Nai - Stree 2 320 Kbps.mp3',
+    'songsplaylist/audio1.mp3',
+    'songsplaylist/Bhool Bhulaiyaa 3 - Title Track (Feat. Pitbull) - Bhool Bhulaiyaa 3 320 Kbps.mp3',
+    'songsplaylist/Phir Se Ud Chala - Rockstar 320 Kbps.mp3',
+    'songsplaylist/Hauli Hauli - Khel Khel Mein 320 Kbps.mp3',
+    'songsplaylist/In Love - Guru Randhawa 320 Kbps.mp3',
+    'songsplaylist/Ittar - Jasmine Sandlas 320 Kbps.mp3',
+    'songsplaylist/Kissik - Pushpa 2 The Rule 320 Kbps.mp3',
+    'songsplaylist/Mera Na - Sidhu Moose Wala 320 Kbps.mp3',
+    'songsplaylist/Saanware - Akhil Sachdeva 320 Kbps.mp3',
+    'songsplaylist/Aaj Ki Raat - Stree 2 320 Kbps.mp3',
+    'songsplaylist/Aayi Nai - Stree 2 320 Kbps.mp3',
+    'songsplaylist/audio1.mp3',
+    'songsplaylist/Bhool Bhulaiyaa 3 - Title Track (Feat. Pitbull) - Bhool Bhulaiyaa 3 320 Kbps.mp3',
+    'songsplaylist/Phir Se Ud Chala - Rockstar 320 Kbps.mp3',
+    'songsplaylist/Hauli Hauli - Khel Khel Mein 320 Kbps.mp3',
+    'songsplaylist/In Love - Guru Randhawa 320 Kbps.mp3',
+    'songsplaylist/Ittar - Jasmine Sandlas 320 Kbps.mp3',
+    'songsplaylist/Kissik - Pushpa 2 The Rule 320 Kbps.mp3',
+    'songsplaylist/Mera Na - Sidhu Moose Wala 320 Kbps.mp3',
+    'songsplaylist/Saanware - Akhil Sachdeva 320 Kbps.mp3'
 ];
 
-// Function to play the selected song
 function playSong(songUrl) {
-    if (isPlaying) {
-        audioPlayer.pause();
+    // If the selected song is not the current one
+    if (audioPlayer.src !== songUrl) {
+        if (!audioPlayer.paused) {
+            audioPlayer.pause(); // Pause the current song
+        }
+
+        // Update the audio source to the new song
+        audioPlayer.src = songUrl;
+        audioPlayer.load(); // Load the new audio file
         isPlaying = false;
-        document.getElementById('play').src = "img/play.svg";  // Change to play icon
     }
 
-    // Set the audio source and start playing the song
-    audioPlayer.src = songUrl;
-    audioPlayer.play();
-    isPlaying = true;
-    document.getElementById('play').src = "img/pause.svg";  // Change to pause icon
+    // Toggle play/pause functionality
+    if (audioPlayer.paused) {
+        // Attempt to play the audio
+        audioPlayer
+            .play()
+            .then(() => {
+                isPlaying = true;
+                document.getElementById('play').src = "img/pause.svg"; // Change to pause icon
+            })
+            .catch((error) => {
+                if (error.name !== "AbortError") {
+                    console.error("Error playing the audio:", error);
+                }
+            });
+    } else {
+        // Pause the audio if it's playing
+        audioPlayer.pause();
+        isPlaying = false;
+        document.getElementById('play').src = "img/play.svg"; // Change to play icon
+    }
 
-    // Update the song information display (you can modify this for your needs)
-    let songTitle = songUrl.split('/').pop().split('.')[0];
-    let songInfo = document.querySelector('.songinfo');
+    // Update song information display
+    const songTitle = songUrl.split('/').pop().split('.')[0];
+    const songInfo = document.querySelector('.songinfo');
     if (songInfo) {
-        songInfo.innerText = songTitle;  // Ensure this element exists
+        songInfo.innerText = songTitle;
     }
 }
 
+
+
 // Function to play the next song
 function nextSong() {
-    currentSongIndex++;
-    if (currentSongIndex >= songsList.length) {
-        currentSongIndex = 0; // Loop back to the first song
-    }
+    currentSongIndex = (currentSongIndex + 1) % songsList.length; // Loop to the first song if at the end
     playSong(songsList[currentSongIndex]);
 }
 
 // Function to play the previous song
 function previousSong() {
-    currentSongIndex--;
-    if (currentSongIndex < 0) {
-        currentSongIndex = songsList.length - 1; // Loop back to the last song
-    }
+    currentSongIndex = (currentSongIndex - 1 + songsList.length) % songsList.length; // Loop to the last song if at the start
     playSong(songsList[currentSongIndex]);
 }
 
-// Attach event listeners for play, previous, and next buttons
+// Attach event listener for play button
 const playButton = document.getElementById('play');
+
 if (playButton) {
     playButton.addEventListener('click', () => {
-        if (isPlaying) {
+        if (audioPlayer.paused) {
+            // Attempt to play the audio
+            audioPlayer
+                .play()
+                .then(() => {
+                    isPlaying = true;
+                    playButton.src = "img/pause.svg"; // Update icon to pause
+                })
+                .catch((error) => {
+                    if (error.name !== "AbortError") {
+                        console.error("Error playing the audio:", error);
+                    }
+                });
+        } else {
+            // Pause the audio
             audioPlayer.pause();
             isPlaying = false;
-            playButton.src = "img/play.svg";  // Change to play icon
-        } else {
-            audioPlayer.play();
-            isPlaying = true;
-            playButton.src = "img/pause.svg";  // Change to pause icon
+            playButton.src = "img/play.svg"; // Update icon to play
         }
     });
 }
+
 
 const nextButton = document.getElementById('next');
 if (nextButton) {
@@ -86,7 +126,37 @@ if (previousButton) {
 // Function to update the volume based on the input range
 const volumeRange = document.querySelector('.range input');
 if (volumeRange) {
-    volumeRange.addEventListener('input', function() {
+    volumeRange.addEventListener('input', function () {
         audioPlayer.volume = this.value / 100;
     });
 }
+
+// Dynamically create the song list
+const songUL = document.querySelector('.songList ul');
+if (songUL) {
+    songsList.forEach((song, index) => {
+        // Create a new list item
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `
+            <img class="invert" src="img/music.svg" alt="music icon">
+            <div class="info">
+                <div>${song.split('/').pop().split('.')[0]}</div>
+                <div>charu</div> <!-- Replace with actual artist names -->
+            </div>
+            <div class="Playnow">
+                <span>Play Now</span>
+                <img class="invert playNowBtn" src="img/play.svg" alt="play">
+            </div>
+        `;
+        listItem.querySelector('.Playnow').addEventListener('click', () => {
+            currentSongIndex = index;
+            playSong(song);
+        });
+        songUL.appendChild(listItem);
+    });
+}
+
+// Handle errors globally for audio
+audioPlayer.addEventListener('error', (e) => {
+    console.error("Audio error occurred:", e);
+});
